678e12c4a52fdb4d2205644f8a60165150b10b6c
unknown <vanshagrawal068@gmail.com>
2026-09-19 22:35:37 +0530

    Cintact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -86,7 +86,7 @@ export default function Contact() {
 
     setApiState('sending');
     setErrorMessage('');
-
+//
     const serviceId = import.meta.env.VITE_EMAILJS_SERVICE_ID;
     const templateId = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
     const publicKey = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
